3c94f203a33e0bc0283798ec399237739e0053ad
Angie <liya0706s@gmail.com>
2024-05-29 15:09:04 +0800

    feat:migrate crud function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,32 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
-    function show(Request $request)
+    // function show(Request $request)
+    // {
+
+    //     $id = $request->input('id');
+
+    //     $products = [
+    //         [
+    //             "image" => asset('images/snoopy.jpg')
+    //         ],
+    //         [
+    //             "image" => asset('images/lucy.jpg')
+    //         ]
+    //     ];
+
+    //     $product= $products[$id];
+
+    //     var_dump($product);
+
+    //     return view('product.show', [
+    //         "product" => $product
+    // ]);
+    // }
+
+    public function create()
     {
-        // order => 訂單
-        // product => 商品
-        // prefix => 前綴
+        return view('product.create');
+    }
 
-        $id = $request->input('id');
+    public function store(Request $request)
+    {
+        DB::table('products')->insert([
+            'name' => 'Product 1',
+            'price' => 100
+        ]);
 
-        $products = [
-            [
-                "image" => asset('images/snoopy.jpg')
-            ],
-            [
-                "image" => asset('images/lucy.jpg')
-            ]
-        ];
-
-        $product= $products[$id];
-
-        // var_dump($product);
-
-        return view('product.show', [
-            "product" => $product
-    ]);
+        return redirect()->route('products.index');
     }
 }
--- database/migrations/2024_05_27_072909_create_products_table.php
+++ database/migrations/2024_05_27_072909_create_products_table.php
@@ -15,7 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->string('img_url')->nullable();
+            $table->integer('price')->default(0);
             $table->timestamps();
+            // column name 
         });
     }
 
